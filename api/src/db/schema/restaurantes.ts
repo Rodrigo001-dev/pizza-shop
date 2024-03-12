e966fea7ce43facfe0b546d358ad3d08e6ccb78b
Rodrigo Rael <rodrigorael53@gmail.com>
2024-03-12 12:34:04 -0300

    :sparkles: feat: database

---
+++ api/src/db/schema/restaurantes.ts
@@ -0,0 +1,28 @@
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { createId } from '@paralleldrive/cuid2'
+import { relations } from 'drizzle-orm'
+
+import { users } from './users'
+
+export const restaurants = pgTable('restaurants', {
+  id: text('id')
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  name: text('name').notNull(),
+  description: text('description'),
+  managerId: text('manager_id').references(() => users.id, {
+    onDelete: 'set null',
+  }),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+})
+
+export const restaurantsRelations = relations(restaurants, ({ one }) => {
+  return {
+    manager: one(users, {
+      fields: [restaurants.managerId],
+      references: [users.id],
+      relationName: 'restaurant_manager',
+    }),
+  }
+})
---
+++ api/src/db/seed.ts
@@ -0,0 +1,68 @@
+/* eslint-disable drizzle/enforce-delete-with-where */
+
+import { faker } from '@faker-js/faker'
+import chalk from 'chalk'
+
+import { restaurants, users } from './schema'
+import { db } from './connection'
+
+/**
+ * Reset database
+ */
+await db.delete(users)
+await db.delete(restaurants)
+
+console.log(chalk.yellowBright('✔️ Database reset!'))
+
+/**
+ * Create customers
+ */
+await db.insert(users).values([
+  {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    role: 'customer',
+  },
+  {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    role: 'customer',
+  },
+])
+
+console.log(chalk.yellowBright('✔️ Created customers!'))
+
+/**
+ * Create manager
+ */
+const [manager] = await db
+  .insert(users)
+  .values([
+    {
+      name: faker.person.fullName(),
+      email: 'admin@admin.com',
+      role: 'manager',
+    },
+  ])
+  .returning({
+    id: users.id,
+  })
+
+console.log(chalk.yellowBright('✔️ Created manager!'))
+
+/**
+ * Create restaurant
+ */
+await db.insert(restaurants).values([
+  {
+    name: faker.company.name(),
+    description: faker.lorem.paragraph(),
+    managerId: manager.id,
+  },
+])
+
+console.log(chalk.yellowBright('✔️ Created restaurant!'))
+
+console.log(chalk.greenBright('Database seeded successfully!'))
+
+process.exit(0)
