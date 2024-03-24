f7a05c777abaf8447d7fec39934e87c39b265ea1
Rodrigo Rael <rodrigorael53@gmail.com>
2024-03-24 09:36:46 -0300

    :sparkles: feat: orders routes

---
+++ api/src/http/routes/get-orders.ts
@@ -0,0 +1,79 @@
+import Elysia, { t } from 'elysia'
+import { and, count, desc, eq, ilike, sql } from 'drizzle-orm'
+
+import { auth } from '../auth'
+import { db } from '../../db/connection'
+import { UnauthorizedError } from '../errors/unauthorized-error'
+import { createSelectSchema } from 'drizzle-typebox'
+import { orders, users } from '../../db/schema'
+
+export const getOrders = new Elysia().use(auth).get(
+  '/orders',
+  async ({ getCurrentUser, query }) => {
+    const { restauranteId } = await getCurrentUser()
+    const { customerName, orderId, status, pageIndex } = query
+
+    if (!restauranteId) {
+      throw new UnauthorizedError()
+    }
+
+    const baseQuery = db
+      .select({
+        orderId: orders.id,
+        createdAt: orders.createdAt,
+        status: orders.status,
+        total: orders.totalInCents,
+        customerName: users.name,
+      })
+      .from(orders)
+      .innerJoin(users, eq(users.id, orders.customerId))
+      .where(
+        and(
+          eq(orders.restaurantId, restauranteId),
+          orderId ? ilike(orders.id, `%${orderId}%`) : undefined,
+          status ? eq(orders.id, status) : undefined,
+          customerName ? ilike(users.name, `%${customerName}%`) : undefined,
+        ),
+      )
+
+    const [amountOfOrdersQuery, allOrders] = await Promise.all([
+      db.select({ count: count() }).from(baseQuery.as('baseQuery')),
+      db
+        .select()
+        .from(baseQuery.as('baseQuery'))
+        .offset(pageIndex * 10)
+        .limit(10)
+        .orderBy((fields) => {
+          return [
+            sql`CASE ${fields.status}
+              WHEN 'pending' THEN 1
+              WHEN 'processing' THEN 2
+              WHEN 'delivering' THEN 3
+              WHEN 'delivered' THEN 4
+              WHEN 'canceled' THEN 99
+            END`,
+            desc(fields.createdAt),
+          ]
+        }),
+    ])
+
+    const amountOfOrders = amountOfOrdersQuery[0].count
+
+    return {
+      orders: allOrders,
+      meta: {
+        pageIndex,
+        perPage: 10,
+        totalCount: amountOfOrders,
+      },
+    }
+  },
+  {
+    query: t.Object({
+      customerName: t.Optional(t.String()),
+      orderId: t.Optional(t.String()),
+      status: t.Optional(createSelectSchema(orders).properties.status),
+      pageIndex: t.Numeric({ minimum: 0 }),
+    }),
+  },
+)
--- api/src/http/server.ts
+++ api/src/http/server.ts
@@ -6,6 +6,12 @@ import { authenticateFromLink } from './routes/authenticate-from-link'
 import { signOut } from './routes/sign-out'
 import { getProfile } from './routes/get-profile'
 import { getManagedRestaurante } from './routes/get-managed-restaurante'
+import { getOrderDetails } from './routes/get-order-details'
+import { approveOrder } from './routes/approve-order'
+import { cancelOrder } from './routes/cancel-order'
+import { deliverOrder } from './routes/deliver-order'
+import { dispatchOrder } from './routes/dispatch-order'
+import { getOrders } from './routes/get-orders'
 
 const app = new Elysia()
   .use(registerRestaurante)
@@ -14,12 +20,21 @@ const app = new Elysia()
   .use(signOut)
   .use(getProfile)
   .use(getManagedRestaurante)
+  .use(getOrderDetails)
+  .use(approveOrder)
+  .use(cancelOrder)
+  .use(deliverOrder)
+  .use(dispatchOrder)
+  .use(getOrders)
   .onError(({ code, error, set }) => {
     switch (code) {
       case 'VALIDATION': {
         set.status = error.status
 
         return error.toResponse()
+      }
+      case 'NOT_FOUND': {
+        return new Response(null, { status: 404 })
       }
       default: {
         console.error(error)
